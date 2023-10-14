149fdb727d05ec2281f6db279ff4b9b9791385d3
Ratnesh4193 <57043581+Ratnesh4193@users.noreply.github.com>
2023-10-15 01:08:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,9 +32,6 @@ if (process.env.NODE_ENV !== "production") {
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// only when ready to deploy
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -44,10 +41,6 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
-// only when ready to deploy
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
